3beae39c9c45cb8595ded37b89f8eaa0b9771dae
Joax2020 <joaquinclaros02@gmail.com>
2026-05-28 13:08:01 -0400

    Añadiendo funcionalidad PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      // Esto le dice al plugin que NO genere un manifest nuevo, 
+      // porque ya tienes el tuyo vinculado en tu index.html
+      manifest: false, 
+      workbox: {
+        // Archivos que se guardarán en caché para funcionar offline
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'] 
+      }
+    })
+  ],
+});
